a1e2c6236666a054781bdb94dac8376c6f35732c
Ten Zhi Yang <tzyinc@hotmail.com>
2016-07-21 16:47:57 +0800

    fixed bug, missing month arr for new update

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ $( document ).ready(function(){
     $(".button-collapse").sideNav();
     $('.modal-trigger').leanModal();
 })
-
+var monthNames = [
+  "January", "February", "March",
+  "April", "May", "June", "July",
+  "August", "September", "October",
+  "November", "December"
+];
 var autoloadDir = "";
 var autoload = false;
 function readIframe() {
